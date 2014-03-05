4af04d69fcdb59ffc174f5370beb9d15e1a69708
Sachith Withana <swsachith@gmail.com>
2014-03-05 10:58:00 -0500

    testing form submission without validation VANG-1

--- src/main/java/org/scigap/vanillagateway/services/NewJobHandler.java
+++ src/main/java/org/scigap/vanillagateway/services/NewJobHandler.java
@@ -2,6 +2,7 @@ package org.scigap.vanillagateway.services;
 
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
+import org.apache.airavata.model.workspace.experiment.Experiment;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -15,16 +16,20 @@ public class NewJobHandler {
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public String createJob(@FormDataParam("file") InputStream uploadedInputStream,
-                            @FormDataParam("file") FormDataContentDisposition fileDetail,@FormDataParam("name") String name) {
+                            @FormDataParam("file") FormDataContentDisposition fileDetail,
+                            @FormDataParam("name") final String name,
+                            @FormDataParam("description") final String description) {
 
+        Experiment experiment = new Experiment();
+        experiment.setName(name);
+        experiment.setDescription(description);
+
+
+        //downloading the files to the server
         String uploadedFileLocation = "/Users/swithana/temp/" + fileDetail.getFileName();
-
-        // save it
         saveToFile(uploadedInputStream, uploadedFileLocation);
 
-        String output = "File uploaded to : " + uploadedFileLocation+ " name: "+name;
-        //   String output = "Form uploading complete   " + "name= " + name + " description= " + description;
-        return output;
+        return "Job Created Successfully" + " \nExperiment Name: "+ name+"\nDescription: "+description;
     }
 
     // save uploaded file to new location
